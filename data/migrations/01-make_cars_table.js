05b4d99f6219e6692d902bce87de7f18af1ca08c
arsalan rahimi <years1373@yahoo.com>
2022-01-18 21:06:44 -0800

    migrations and seeds completed

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
-};
+  return knex.schema.createTable("cars", (table) => {
+    table.increments("id")
+    table.string("vin").notNullable().unique()
+    table.string("make").notNullable()
+    table.string("model").notNullable()
+    table.integer("mileage").notNullable()
+    table.string("title").defaultTo(null)
+    table.string("transmission").defaultTo(null)
+  })
+}
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
-};
+  return knex.schema.dropTableIfExists("cars")
+}
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,42 @@
 // STRETCH
+exports.seed = function (knex) {
+  return knex("cars")
+    .truncate()
+    .then(() => {
+      return knex("cars").insert([
+        {
+          vin: "11111111",
+          make: "car1 Make",
+          model: "car1 Model",
+          mileage: 100,
+          title: "awesome car",
+          transmission: " six gear ",
+        },
+
+        {
+          vin: "22222222",
+          make: "car2 Make",
+          model: "car2 Model",
+          mileage: 200,
+          title: "not awesome car",
+          transmission: " six gear ",
+        },
+        {
+          vin: "333333333",
+          make: "car3 Make",
+          model: "car3 Model",
+          mileage: 300,
+          title: "meh car",
+          transmission: " six gear ",
+        },
+        {
+          vin: "444444444",
+          make: "car4 Make",
+          model: "car4 Model",
+          mileage: 400,
+          title: "not even meh car",
+          transmission: " six gear ",
+        },
+      ])
+    })
+}
