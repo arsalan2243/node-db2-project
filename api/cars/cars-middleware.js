0e5a7592187323962473670fa0ad5d333c425be0
arsalan rahimi <years1373@yahoo.com>
2022-01-18 22:00:24 -0800

    finished project

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,73 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const { getById, getAll } = require("./cars-model")
+const vinValidator = require("vin-validator")
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const exist = await getById(req.params.id)
+    if (!exist) {
+      next({
+        status: 404,
+        message: `car with id ${req.params.id} is not found`,
+      })
+    } else {
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
 }
 
-const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkCarPayload = async (req, res, next) => {
+  const err = { message: "" }
+  try {
+    const { vin, make, model, mileage } = req.body
+    if (!vin) {
+      err.message = "vin is missing"
+    }
+    if (!make) {
+      err.message = "make is missing"
+    }
+    if (!model) {
+      err.message = "model is missing"
+    }
+    if (!mileage) {
+      err.message = "mileage is missing"
+    }
+    if (err.message) {
+      next({ status: 400, message: err.message })
+    } else {
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
 }
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberValid = async (req, res, next) => {
+  try {
+    if (!vinValidator.validate(req.body.vin)) {
+      next({ status: 400, message: `vin ${req.body.vin} is invalid` })
+    } else next()
+  } catch (error) {
+    next(error)
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  try {
+    const cars = await getAll()
+    const valid = cars.some((car) => car.vin === req.body.vin)
+    if (valid) {
+      next({ status: 400, message: `vin ${req.body.vin} already exists` })
+    } else next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
 }
--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,34 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require("../../data/db-config")
+
+const getAll = async () => {
+  return db("cars")
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = async (id) => {
+  const car = await db("cars").where("id", id).first()
+  return car
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = async (car) => {
+  const [id] = await db("cars").insert(car)
+  return getById(id)
+}
+
+const update = async (id, car) => {
+  await db("cars").where("id", id).update(car)
+  return getById(id)
+}
+
+const remove = async (id) => {
+  const deletedCar = await getById(id)
+  await db("cars").where("id", id).del()
+  return deletedCar
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
 }
