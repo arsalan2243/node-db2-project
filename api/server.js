5073d86a5c551ef862a4a5e1954b7123e2d54e15
arsalan rahimi <years1373@yahoo.com>
2022-01-18 21:41:28 -0800

    model and router completed

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require("express")
-
+const carsRouter = require("./cars/cars-router")
 const server = express()
+server.use(express.json())
+server.use("/api/cars", carsRouter)
+server.use("*", (req, res, next) => {
+  next({ status: 404, message: "Not Found" })
+})
+server.use((err, req, res, next) => {
+  // eslint-disable-line
 
-// DO YOUR MAGIC
-
+  res.status(err.status || 500).json({ message: err.message })
+})
 module.exports = server
